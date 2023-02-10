94312c2ec83c2db85fc68803563c6292d158c4cf
Jorge Martinez <tnlcomputer@gmail.com>
2023-02-10 18:56:20 -0300

    first commit

---
+++ objetos.py
@@ -0,0 +1,18 @@
+class miclase:
+  def suma(self, a, b)
+  return a + b
+
+
+a=5 #integer
+b="hola" #string
+c=2.2 #double
+d=True #boolean
+e=[1,2,3,4,5] #list
+f=miclase()
+print(f.suma(a,b))
+print(f.suma(a,b))
+
+
+type(a)
+print(a)
+
