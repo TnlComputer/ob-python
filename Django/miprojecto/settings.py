cf9f0293229755756ac9ce8e6850adbc634d9d75
Jorge Martinez <tnlcomputer@gmail.com>
2023-02-15 17:16:13 -0300

    sacando contraseña

--- Django/miprojecto/settings.py
+++ Django/miprojecto/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dbprueba',
         'USER': 'postgres',
-        'PASSWORD': 'facil123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
